7ddad55906940cf5fc1c0d67e0b20ef77f84f03f
Dhananjay Kumar <dkumarx@users.noreply.github.com>
2020-06-19 09:42:26 +1000

    Initial React project startup code checked in

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">Showing pets (Cat) : Sort by Gender's owner</header>
+        <main>
+          Result container
+        </main>
+    </div>
+  );
+}
+
+export default App;
